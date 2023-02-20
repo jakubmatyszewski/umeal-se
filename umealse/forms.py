a3324433448295ec339fd160b632e67756254c2d
jakubmatyszewski <jakubmatyszewski1@gmail.com>
2023-02-20 14:25:05 +0100

    Feat: Ensure users use unique emails

--- umealse/forms.py
+++ umealse/forms.py
@@ -21,6 +21,12 @@ class UserRegistrationForm(forms.ModelForm):
         if cd["password"] != cd["re_password"]:
             raise forms.ValidationError("Passwords don't match.")
         return cd["re_password"]
+    
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        if User.objects.filter(email=data).exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 
 class UserEditForm(forms.ModelForm):
@@ -28,6 +34,12 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ["first_name", "email"]
 
+    def clean_email(self):
+        data = self.cleaned_data['email']
+        queryset = User.objects.exclude(id=self.instance.id).filter(email=data)
+        if queryset.exists():
+            raise forms.ValidationError('Email already in use.')
+        return data
 
 class ProfileEditForm(forms.ModelForm):
     class Meta:
